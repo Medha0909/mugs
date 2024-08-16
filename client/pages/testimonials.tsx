27401ed11c8d5690ed85289fdd6d1e4775621b6c
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-08-16 09:25:54 -0700

    Update testimonials.tsx

--- client/pages/testimonials.tsx
+++ client/pages/testimonials.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/testimonials.module.css";
 
 function testimonials() {
   return (
-    <div id="testimonals">
+    <div id="testimonials">
       <div className={styles.te1}>
         <div className={styles.te2}>
           <span className={styles.tes}>Testimonials</span>
